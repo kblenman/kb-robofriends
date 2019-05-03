3950f7bfde3f5ac8239092190ab0229a12429b59
kblenman <kira-blenman@hotmail.com>
2019-05-03 16:02:35 -0400

    Updates

---
+++ precache-manifest.48bc714f988efd497c5f7819d17ae180.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/kb-robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "c7df2e86c112203ae424",
+    "url": "/kb-robofriends/static/js/runtime~main.8f2ecc9c.js"
+  },
+  {
+    "revision": "351c4224e2ec649876e8",
+    "url": "/kb-robofriends/static/js/main.036168a6.chunk.js"
+  },
+  {
+    "revision": "e4b280d20a6163c3d25f",
+    "url": "/kb-robofriends/static/js/2.4d43d901.chunk.js"
+  },
+  {
+    "revision": "351c4224e2ec649876e8",
+    "url": "/kb-robofriends/static/css/main.4af2bd9c.chunk.css"
+  },
+  {
+    "revision": "e4b280d20a6163c3d25f",
+    "url": "/kb-robofriends/static/css/2.ec460a3b.chunk.css"
+  },
+  {
+    "revision": "31a259dfbe7cb76ad8b0acda84fa3936",
+    "url": "/kb-robofriends/index.html"
+  }
+];
